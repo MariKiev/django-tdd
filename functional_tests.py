392cc01283c88c39714b5fea02b88d02d2d82a7e
Marina Beskorovaina <iam.marik@gmail.com>
2016-01-17 00:43:10 +0700

    functional test checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -14,19 +15,30 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter to to-do item'
+                         )
+
+        # She types "Buy fruits" into a text box
+        inputbox.send_keys('Buy fruits')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy fruits" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == "1: Buy fruits" for row in rows))
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test')
-
-    # She is invited to enter a to-do item straight away
-
-    # She types "Buy peacock feathers" into a text box (Edith's hobby
-    # is tying fly-fishing lures)
-
-    # When she hits enter, the page updates, and now the page lists
-    # "1: Buy peacock feathers" as an item in a to-do list
-
-    # There is still a text box inviting her to add another item. She
-    # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
     # The page updates again, and now shows both items on her list
 
