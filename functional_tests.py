a8d24ede25a732ff71c01244b5108cd95723c2e1
Marina Beskorovaina <iam.marik@gmail.com>
2016-01-20 19:47:13 +0700

    added model for list Items

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -26,23 +31,22 @@ class NewVisitorTest(unittest.TestCase):
                          )
 
         # She types "Buy fruits" into a text box
-        # inputbox.send_keys("Buy fruits")
-        inputbox.send_keys("Water")
+        inputbox.send_keys("Buy fruits")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy fruits" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy fruits", [row.text for row in rows])
-        self.assertIn("2: Water", [row.text for row in rows])
+        self.check_for_row_in_table("1: Buy fruits")
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test')
+        # enters "Water" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Water")
+        inputbox.send_keys(Keys.ENTER)
 
-    # The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_table("1: Buy fruits")
+        self.check_for_row_in_table("2: Water")
 
     # Edith wonders whether the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
