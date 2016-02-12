a69c1584ca13f0f73f924d9557cb0b3cccdf0540
Marina Beskorovaina <iam.marik@gmail.com>
2016-02-12 11:09:35 +0700

    added requirements

--- lists/models.py
+++ lists/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default="")
+    list = models.TextField(default="")
 
 
+class List(models.Model):
+    pass
 
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from models import Item
 
 
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect('/lists/user-1/')
     return render(request, "home.html", )
 
 
@@ -14,3 +10,7 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, "list.html", {'items': items})
 
+
+def new_list(request):
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect('/lists/user-1/')
